31b826d713fc2ad07e11c018eb6b6c36d07b3143
Emil Soleymani <soleymae@mcmaster.ca>
2022-08-30 15:56:22 -0400

    Added routing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import Log from "./Components/Log";
 import Programs from "./Components/Programs";
 import Welcome from "./Components/Welcome";
 
+import { Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
-      <Programs/>
+      <Routes>
+        <Route path="/" element={<Welcome/>}/>
+        <Route path="/log" element={<Log/>}/>
+        <Route path="/programs" element={<Programs/>}/>
+      </Routes>
       <Header/>
       <Footer/>
     </div>
