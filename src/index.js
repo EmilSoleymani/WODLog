daa5ba9d6aa0800763647a31d2d2ef930f616bcb
Emil Soleymani <soleymae@mcmaster.ca>
2022-09-08 14:30:59 -0400

    Tried to add sentry

--- src/index.js
+++ src/index.js
@@ -4,18 +4,46 @@ import reportWebVitals from './reportWebVitals';
 
 import App from './App'
 
-import { BrowserRouter } from 'react-router-dom'
+import {
+  Routes,
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://ce2183279fe1452c8c4b5ac72f439ee7@o1401150.ingest.sentry.io/6731502",
+  integrations: [
+      new BrowserTracing({
+          routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+              React.useEffect,
+              useLocation,
+              useNavigationType,
+              createRoutesFromChildren,
+              matchRoutes,
+          ),
+      }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes)
+
+ReactDOM.render(
+  <BrowserRouter>
+      <SentryRoutes>
+        <App/>
+      </SentryRoutes>
+  </BrowserRouter>,
+  document.getElementById("root"),
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
